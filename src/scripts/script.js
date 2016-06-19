9207373c5a56a70b7391bd74448d9c7bbbad07ba
Ricky Januari <ricky010193@gmail.com>
2016-06-18 21:12:57 -0700

    add Slider

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,3 +7,28 @@
    });
    
 })(jQuery);
+
+$(document).ready(function(){
+
+	(function($) {
+
+		$('#header__icon').click(function(e){
+			e.preventDefault();
+			$('body').toggleClass('with--sidebar');
+		});
+    
+    $('#site-cache').click(function(e){
+      $('body').removeClass('with--sidebar');
+    });
+
+	})(jQuery);
+
+});
+
+$(document).ready(function(){
+     		$('#slider').bxSlider({
+    		ticker: true,
+    		tickerSpeed: 5000,
+			tickerHover: true
+  		});
+  		});
