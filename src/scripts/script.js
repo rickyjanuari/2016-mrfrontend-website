b6036790b8755cfa33fd466c6a0c7a79932ae3b4
Ricky Januari <ricky010193@gmail.com>
2016-06-20 15:47:17 -0700

    fix padding

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -32,3 +32,31 @@ $(document).ready(function(){
 			tickerHover: true
   		});
   		});
+
+//TIMELINE GUILINE
+jQuery(document).ready(function($){
+	var timelineBlocks = $('.guidelines-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame)
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.guidelines-img, .guidelines-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.guidelines-img').hasClass('is-hidden') ) && $(this).find('.guidelines-img, .guidelines-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+});
